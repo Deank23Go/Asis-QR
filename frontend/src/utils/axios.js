11edcf7722a6279e957302ab12df7b76b95a3ac1
Deank23GO <deangonzalez23@gmail.com>
2025-07-19 21:57:51 -0500

    nuevos cambios en el .env de front

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-
+;
 // Aquí configuramos la URL base (el backend de Node.js)
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',  // URL de tu backend (ajústala si usas otro puerto o dominio)
+  baseURL: import.meta.env.VITE_AXIOS,  // URL de tu backend (ajústala si usas otro puerto o dominio)
   timeout: 10000,  // Configura un tiempo de espera de 5 segundos
   headers: {
     'Content-Type': 'application/json', // Establece el tipo de contenido para solicitudes JSON
