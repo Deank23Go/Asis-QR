0a0172742cadaf820a961b0f5854de5c6b962469
Deank23GO <deangonzalez23@gmail.com>
2025-07-22 13:00:00 -0500

    Corrige el formato del archivo _redirects y ajusta la configuración de Vite para la base del proyecto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,11 @@ import path from 'path'  // Para trabajar con rutas absolutas
 
 // Configuración de Vite
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],  // Añadir los plugins para React y Tailwind CSS
   resolve: {
     alias: {
+      // eslint-disable-next-line no-undef
       '@': path.resolve(__dirname, 'src'),  // Alias '@' apunta a la carpeta 'src'
     },
   },
